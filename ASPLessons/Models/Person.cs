b4c0032a34b9d3c3c3ab1d7b14b8a7585fa3744a
pinkxpink00 <kyperu07@gmail.com>
2023-12-11 22:26:01 +0200

    delete pass

--- ASPLessons/Models/Person.cs
+++ ASPLessons/Models/Person.cs
@@ -4,19 +4,16 @@ namespace ASPLessons.Models
 {
     public class Person
     {
-        [Required(ErrorMessage = "Name must required")]
-        [StringLength(50, MinimumLength = 3, ErrorMessage = "Длина строки должна быть от 3 до 50 символов")]
+        [Required(ErrorMessage = "Не указано имя")]
         public string? Name { get; set; }
-        [Range(1, 110, ErrorMessage = "Недопустимый возраст")]
-        public int Age { get; set; }
-        [RegularExpression(@"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}", ErrorMessage = "Некорректный адрес")]
+
+        [Required(ErrorMessage = "Не указан электронный адрес")]
         public string? Email { get; set; }
-        [Required]
-        public string? Password { get; set; }
 
-        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
-        public string? PasswordConfirm { get; set; }
+        [Required(ErrorMessage = "Не указан возраст")]
+        [Range(1, 100)]
+        public int Age { get; set; }
 
-
+        public int Password { get; set; }
     }
 }
