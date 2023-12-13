bf1e12e159113a980c0b0e602773eb06d3967490
pinkxpink00 <kyperu07@gmail.com>
2023-12-13 23:14:49 +0200

    create RouteHelpers

---
+++ ASPLessons/Infrastracture/Helpers/RouteHelper.cs
@@ -0,0 +1,25 @@
+﻿using ASPLessons.Models;
+
+namespace ASPLessons.Infrastracture.Helpers
+{
+    public class RouteHelper
+    {
+        public void GetCurrentRoute(RouteData routeData)
+        {
+            var route = RouteResponse.Routes
+                .FirstOrDefault(x => x.Action == routeData.Values["Action"]
+                                     && x.Controller == routeData.Values["Controller"]);
+
+            if (route == null)
+            {
+                var routeResponce = new RouteResponse()
+                {
+                    Controller = routeData.Values["controller"]?.ToString(),
+                    Action = routeData.Values["action"]?.ToString(),
+                };
+
+                RouteResponse.Routes.Add(routeResponce);
+            }
+        }
+    }
+}
